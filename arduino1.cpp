0e4b33bfb3a14ca5f4986e85c75cf14e8def3e1d
Anna Zheng <anna.zheng@ucf.edu>
2024-09-23 20:28:03 -0400

    Update arduino1.cpp
    added comments

--- arduino1.cpp
+++ arduino1.cpp
@@ -1,16 +1,18 @@
 // C++ code
+// Make an LED blink on and off every second.
 
-const int led = 13;
+const int led = 13;  // LED is connected to pin 13
 
 void setup()
 {
-  pinMode(led, OUTPUT);
+  pinMode(led, OUTPUT);  // sets pin 13 as an output
 }
 
 void loop()
 {
-  digitalWrite(led, HIGH);
+  digitalWrite(led, HIGH); // turns the LED on (HIGH = sends a voltage to the pin)
   delay(1000); // Wait for 1000 millisecond(s)
-  digitalWrite(led, LOW);
+  
+  digitalWrite(led, LOW); // turns the LED off (LOW = stops the voltage to the pin)
   delay(1000); // Wait for 1000 millisecond(s)
 }
